bad7e4a6a0d42e30d38d36b9511c7936d5dc75cb
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-07-26 20:44:57 +0300

    HackerRank

--- HakerRankTask/Task 02/StartUp.cs
+++ HakerRankTask/Task 02/StartUp.cs
@@ -27,7 +27,12 @@ class Solution
 {
     public static void Main(string[] args)
     {
-        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+    //Given a time in 12-hour AM/PM format, convert it to military (24-hour) time.
+    //Note: -12:00:00AM on a 12 - hour clock is 00:00:00 on a 24 - hour clock.
+    //- 12:00:00PM on a 12 - hour clock is 12:00:00 on a 24 - hour clock.
+
+            TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
 
         string s = Console.ReadLine();
 
